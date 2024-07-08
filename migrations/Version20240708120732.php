5f09ab5306dfb710e684ac10d0262648ab70ba6f
lodasck <raven.dev.m@gmail.com>
2024-07-08 14:10:42 +0200

    config .env and .env.local

--- migrations/Version20240708120732.php
+++ migrations/Version20240708120732.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240708082133 extends AbstractMigration
+final class Version20240708120732 extends AbstractMigration
 {
     public function getDescription(): string
     {
